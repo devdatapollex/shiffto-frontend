2078e9a381bccbabe87ebc2952200d57348c74ac
gazifayaz <gazifayaz.16694@gmail.com>
2026-07-23 16:19:11 +0600

    style(ui): enforce text-foreground! and cursor-pointer in base pagination component

--- components/ui/pagination.tsx
+++ components/ui/pagination.tsx
@@ -46,6 +46,7 @@ function PaginationLink({ className, isActive, size = 'icon', ...props }: Pagina
           variant: isActive ? 'outline' : 'ghost',
           size,
         }),
+        'text-foreground! cursor-pointer',
         className
       )}
       {...props}
