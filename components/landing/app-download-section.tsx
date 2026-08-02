339682784e3afd06da7d79dc880533cca849bc30
gazifayaz <gazifayaz.16694@gmail.com>
2026-08-02 15:00:33 +0600

    refactor: remove unoptimized prop from device mock Image components

--- components/landing/app-download-section.tsx
+++ components/landing/app-download-section.tsx
@@ -55,7 +55,6 @@ export function AppDownloadSection() {
             alt="Shiftto Laptop App Dashboard"
             className="w-full h-auto object-contain"
             priority
-            unoptimized
           />
         </div>
 
@@ -67,7 +66,6 @@ export function AppDownloadSection() {
             width={180}
             height={360}
             className="w-full h-auto object-contain drop-shadow-2xl"
-            unoptimized
           />
         </div>
 
@@ -79,7 +77,6 @@ export function AppDownloadSection() {
             width={410}
             height={310}
             className="w-full h-auto object-contain drop-shadow-2xl"
-            unoptimized
           />
         </div>
       </div>
